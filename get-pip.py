82f498482d188bf5af572de2ceede5e2accbbd41
sssalas09 <shereena.salas@bse.eu>
2026-03-29 21:23:26 +0800

    make this windows compatible at least in venv

---
+++ get-pip.py
@@ -0,0 +1,12 @@
+<html>
+<head><title>Index of /</title></head>
+<body>
+<h1>Index of /</h1><hr><pre><a href="../">../</a>
+<a href="pip/">pip/</a>                                               06-Feb-2026 03:13                   -
+<a href="virtualenv/">virtualenv/</a>                                        09-Mar-2026 17:34                   -
+<a href="bootstrap-buildout.py">bootstrap-buildout.py</a>                              18-Nov-2024 17:04                7458
+<a href="ez_setup.py">ez_setup.py</a>                                        18-Nov-2024 17:04               12537
+<a href="get-pip.py">get-pip.py</a>                                         06-Feb-2026 03:13             2193439
+<a href="virtualenv.pyz">virtualenv.pyz</a>                                     09-Mar-2026 17:34             8387875
+</pre><hr></body>
+</html>
